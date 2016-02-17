3b55db08f1851506153f257f7b8e5f04d2fc5440
黃紋吸蜜 <no134217728@gmail.com>
2016-02-17 17:59:34 +0800

    init commit

---
+++ LinkedinLikeDemoSWIFT/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  LinkedinLikeDemoSWIFT
+//
+//  Created by Gelbhaubenkakadu on 2016/2/16.
+//  Copyright © 2016年 iOSatTaipeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet var imgBackground: UIImageView!
+    @IBOutlet var mainScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainScrollView.delegate = self;
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        print(scrollView.contentOffset.y)
+        imgBackground.alpha = 1 - (0.5 / 150 * scrollView.contentOffset.y)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
